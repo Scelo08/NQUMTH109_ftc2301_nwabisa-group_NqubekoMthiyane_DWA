c075bc329f6c15fde72908bbebab828934feb0e1
Nqubeko <yandibongi001@gmail.com>
2023-06-06 01:29:20 +0200

    User story 3 solved

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -26,5 +26,22 @@ if(!dividend || !divider){
     warning.innertext = ""; 
     result.innerText = Math.floor(dividend / divider);
 }
+
+/**
+ * This checks if the divider is below zero and stops the calculation.
+ */
+if(divider < 0){
+    warning.innerText = "Division not performed. Invalid number provided. Try again.";
+    //result.innerText = "";
+
+}else{
+    warning.innerText = "";
+    try {
+        result.innerText = Math.floor(dividend / divider);
+      } catch (error) {
+        console.error("An error occurred:", error);
+        console.error("Call stack:", error.stack);
+      }
+}
 });
  

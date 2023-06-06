cd0aa5970623063a7a069342bd47984b0ce292e4
Nqubeko <yandibongi001@gmail.com>
2023-06-06 14:02:51 +0200

    Changes

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -4,7 +4,7 @@
  */
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-const warning = document.querySelector("[data-warning]")
+const warning = document.querySelector("[data-warning]");
 
 /**
  * Event listener for form submission.
@@ -16,43 +16,43 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-/**
- * This checks if either dividend or divider is empty.
- *  */ 
-if(!dividend || !divider){
+  /**
+   * This checks if either dividend or divider is empty.
+   *  */
+  if (dividend === "" || divider === "") {
     warning.innerText = "Division not performed. Both values are required in inputs. Try again.";
-    result.innertext = ""; //result is cleared by setting result.innertext to an empty string.
-}else{
-    warning.innertext = ""; 
+    //result.innerText = ""; //result is cleared by setting result.innertext to an empty string.
+  }  else {
+     warning.innerText = "";
     result.innerText = Math.floor(dividend / divider);
-}
+   }
 
-/**
- * This checks if the divider is below zero and stops the calculation.
- */
-if(divider < 0){
+  /**
+   * This checks if the divider is below zero and stops the calculation.
+   */
+  if (divider < 0) {
     warning.innerText = "Division not performed. Invalid number provided. Try again.";
     //result.innerText = "";
+  // } else {
+  //   warning.innerText = "";
+  //   try {
+  //     result.innerText = Math.floor(dividend / divider);
+  //   } catch (error) {
+  //     console.error("An error occurred:", error);
+  //     console.error("Call stack:", error.stack);
+  //   }
 
-}else{
-    warning.innerText = "";
-    try {
-        result.innerText = Math.floor(dividend / divider);
-      } catch (error) {
-        console.error("An error occurred:", error);
-        console.error("Call stack:", error.stack);
-      }
+    /**
+     * Here we condition the code display a full screen error message
+     * and browser error if the inputs YOLO and +++ are used as dividend and
+     * divider respectively.
+     */
+    if (dividend === "YOLO" && divider === "+++") {
+      document.body.innerHTML =
+        "Something critical went wrong. Please reload the page.";
+      console.error("An error occurred: Something critical went wrong.");
+    }
+  }
+ });
 
-/**
- * Here we condition the code display a full screen error message
- * and browser error if the inputs YOLO and +++ are used as dividend and
- * divider respectively.
-  */      
- if (dividend === 'YOLO' && divider === '+++') {
-        document.body.innerHTML = "Something critical went wrong. Please reload the page.";
-        console.error("An error occurred: Something critical went wrong.");      
- }
-}
-});
- 
-//I am aware that I could have used else IF statements for better structure but this is more understanderable.
+// //I am aware that I could have used else IF statements for better structure but this is more understanderable for me.
--- DWA6/scripts.js
+++ DWA6/scripts.js
@@ -235,4 +235,4 @@ document.querySelector('[data-list-items]').addEventListener('click', (event) =>
         document.querySelector('[data-list-subtitle]').innerText = `${authors[active.author]} (${new Date(active.published).getFullYear()})`
         document.querySelector('[data-list-description]').innerText = active.description
     }
-})
+}) 

790bf95c90b67bf2e5c42bae6dc829d158656523
Nqubeko <yandibongi001@gmail.com>
2023-06-26 15:14:49 +0200

    Done

--- DWA8/scripts.js
+++ DWA8/scripts.js
@@ -5,27 +5,56 @@ import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 let page = 1;
 let matches = books
 
-const starting = document.createDocumentFragment()
+// const starting = document.createDocumentFragment()
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
+// for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+//     const element = document.createElement('button')
+//     element.classList = 'preview'
+//     element.setAttribute('data-preview', id)
 
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
+//     element.innerHTML = `
+//         <img
+//             class="preview__image"
+//             src="${image}"
+//         />
         
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
+//         <div class="preview__info">
+//             <h3 class="preview__title">${title}</h3>
+//             <div class="preview__author">${authors[author]}</div>
+//         </div>
+//     `
 
-    starting.appendChild(element)
+//     starting.appendChild(element)
+// }
+
+//DWA9
+class BookPreview extends HTMLElement {
+  connectedCallback() {
+    const starting = document.createDocumentFragment();
+
+    for (const { author, id, image, title } of books.slice(0, BOOKS_PER_PAGE)) {
+      const element = document.createElement('button');
+      element.classList = 'preview';
+      element.setAttribute('data-preview', id);
+
+      element.innerHTML = `
+        <img class="preview__image" src="${image}" />
+        <div class="preview__info">
+          <h3 class="preview__title">${title}</h3>
+          <div class="preview__author">${authors[author]}</div>
+        </div>
+      `;
+
+      starting.appendChild(element);
+    }
+/////////////////////////////////////////////////////////////////
+
+
+    this.appendChild(starting);
+  }
 }
+
+customElements.define('book-preview', BookPreview);
 
 document.querySelector('[data-list-items]').appendChild(starting)
 
